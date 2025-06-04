5167def8f2271b47e738903fe6170a0df3773df0
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2025-06-04 15:43:42 +0200

    Print an error and return instead of raising a exception

--- InstantID.py
+++ InstantID.py
@@ -277,14 +277,15 @@ class ApplyInstantID:
 
         face_embed = extractFeatures(insightface, image)
         if face_embed is None:
-            raise Exception('Reference Image: No face detected.')
+            print(f"\033[33m[InstantID] No face detected in input image. Skipping processing.\033[0m")
+            return model, positive, negative
 
         # if no keypoints image is provided, use the image itself (only the first one in the batch)
         face_kps = extractFeatures(insightface, image_kps if image_kps is not None else image[0].unsqueeze(0), extract_kps=True)
 
         if face_kps is None:
-            face_kps = torch.zeros_like(image) if image_kps is None else image_kps
-            print(f"\033[33mWARNING: No face detected in the keypoints image!\033[0m")
+            print(f"\033[33m[InstantID] No face kps detected in input image. Skipping processing.\033[0m")
+            return model, positive, negative
 
         clip_embed = face_embed
         # InstantID works better with averaged embeds (TODO: needs testing)
